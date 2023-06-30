7b7dde0964b414ea04acaa84d454996b9d3f77fb
Key Yu Wan <keyflcbyuwan@gmail.com>
2023-06-29 23:15:24 -0300

    lawyers

--- src/app/advogados/page.tsx
+++ src/app/advogados/page.tsx
@@ -1,3 +1,28 @@
+import { Banner } from '@/components/Banner'
+import { LawyerCard } from '@/components/LawyerCard'
+import { lawyers } from '@/utils/lawyers'
+
 export default function Lawyers() {
-  return <h1 className="text-zinc-900">Services</h1>
+  return (
+    <>
+      <Banner
+        label="Equipe"
+        title="Conheça nossos advogados"
+        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+      />
+
+      <section className="px-6 py-12 lg:py-24">
+        <div className="grid grid-cols-1 gap-6 md:grid-cols-2 lg:mx-auto lg:max-w-[1120px] lg:grid-cols-4">
+          {lawyers.map((lawyer) => (
+            <LawyerCard
+              key={lawyer.id}
+              name={lawyer.name}
+              occupation={lawyer.occupation}
+              photoUrl={lawyer.photoUrl}
+            />
+          ))}
+        </div>
+      </section>
+    </>
+  )
 }
--- src/components/HomeSections/Team.tsx
+++ src/components/HomeSections/Team.tsx
@@ -1,44 +1,6 @@
 import { SectionLabel } from '@/components/SectionLabel'
 import { LawyerCard } from '../LawyerCard'
-
-import advogado01Img from '@/assets/lawyers/advogado-1.jpg'
-import advogado02Img from '@/assets/lawyers/advogado-2.jpg'
-import advogado03Img from '@/assets/lawyers/advogado-3.jpg'
-import advogado04Img from '@/assets/lawyers/advogado-4.jpg'
-
-interface Lawyer {
-  id: string
-  name: string
-  occupation: string
-  photoUrl: string
-}
-
-const lawyers: Lawyer[] = [
-  {
-    id: '1',
-    name: 'Rafael Cooper',
-    occupation: 'Advogado de Família',
-    photoUrl: advogado01Img.src,
-  },
-  {
-    id: '2',
-    name: 'Felipe Davis',
-    occupation: 'Advogado de Negócios',
-    photoUrl: advogado02Img.src,
-  },
-  {
-    id: '3',
-    name: 'Henry Miller',
-    occupation: 'Advogado de Seguro',
-    photoUrl: advogado03Img.src,
-  },
-  {
-    id: '4',
-    name: 'Carlos Stevens',
-    occupation: 'Advogado Criminal',
-    photoUrl: advogado04Img.src,
-  },
-]
+import { lawyers } from '@/utils/lawyers'
 
 export function Team() {
   return (
