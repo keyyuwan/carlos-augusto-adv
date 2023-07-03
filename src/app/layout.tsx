c7769ff9593bc7702944bc4021da76565bf53ca2
Key Yu Wan <keyflcbyuwan@gmail.com>
2023-07-03 16:34:28 -0300

    fix: no-unsued-vars

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@ import { ReactNode } from 'react'
 import { Inter, Libre_Baskerville as LibreBaskerville } from 'next/font/google'
 
 import { Header } from '@/components/Header'
-import { Footer } from '@/components/Footer'
 import './globals.css'
 
 const inter = Inter({ subsets: ['latin'], variable: '--font-inter' })
